dc2feb74f7ff8484009d89b86e2d04b6aba573f9
Nurumbet-ux <saryevnur@gmail.com>
2020-11-25 14:31:35 +0600

    com

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
 
 public class Main {
-    public static int constant = 6000;
+
     public static void main(String[] args) {
+        int constant = 6000;
+        Scanner sc = new Scanner(System.in);
         BankAccount b = new BankAccount(20000);
-
-          while (true){
-
-              if (b.getAmount()==0){
-                  break;
-              }else{
-                  b.withDraw(constant);
-              }
-          }
-
+        while (true) {
+            if (b.getAmount() <= 0) {
+                break;
+            } else if (b.getAmount() >= constant) {
+                b.withDraw(constant);
+            } else {
+                try {
+                    throw new LimitException("Недостаточно средств!", b.getAmount());
+                } catch (LimitException e) {
+                    System.out.println(e.getMessage() + "\nвы можете снять: " + e.getRemainingAmount());
+                    System.out.println("сумма: " );
+                    constant= sc.nextInt();
+                }
+            }
+        }
     }
 }
+
+
+
